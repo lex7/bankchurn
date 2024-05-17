dd1dde182db0284067d78d1c8f934832e178f661
Lex7 <alexray9228@gmail.com>
2024-05-17 15:06:17 +0500

    Веб Приложение

--- app.py
+++ app.py
@@ -1,44 +1,59 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pandas as pd
-import xgboost as xgb
+from catboost import CatBoostClassifier
 import joblib
 
 app = Flask(__name__)
 
-# Load the pre-trained XGBoost model
-xgb_model = xgb.Booster()
-xgb_model.load_model("xgb_model.json")
+# загрузка предобученной модели CatBoost
+catboost_model = CatBoostClassifier()
+catboost_model.load_model("cb_model.json")
 
-# Load the pre-fitted MinMaxScaler
-scaler = joblib.load('min_max_scaler.save') 
+# загрузка обученного Min-Max Scaler и One-Hot Encoder
+scaler = joblib.load('min_max_scaler.save')
 encoder = joblib.load('one_hot_encoder.save')
 
-# Define preprocessing function
+# функция предобработки
 def preprocess_data(data):
-    # Convert 'Age' field to integer
-    data['Age'] = data['Age'].astype(int)
 
-    # Preprocess the data similar to how you did before training
-    data['TargetAge'] = (56 - np.abs(data['Age'] - 56)) / 56 
-    
-    # One-Hot encoding
-    encoded_data = encoder.transform(data[['Geography', 'Gender', 'NumOfProducts']])
-    encoded_df = pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out(['Geography', 'Gender', 'NumOfProducts']), index=data.index)
+    # Перевод строк в целые числа
+    numeric_fields = ['Age', 'NumOfProducts', 'CreditScore', 'EstimatedSalary', 'Balance', 'Tenure']
+    for field in numeric_fields:
+        data[field] = data[field].astype(int)
+
+    # Генерация признака
+    data['TargetAge'] = (56 - np.abs(data['Age'] - 56)) / 56
+
+    # One-Hot Encoding
+    columns_to_encode = ['Geography', 'Gender', 'NumOfProducts']
+    encoded_data = encoder.transform(data[columns_to_encode])
+
+    encoded_columns = [
+        'Geography_France','Geography_Germany','Geography_Spain', 
+        'Gender_Male',
+        'NumOfProducts_1','NumOfProducts_2','NumOfProducts_3','NumOfProducts_4'
+    ]
+    encoded_df = pd.DataFrame(encoded_data, columns=encoded_columns, index=data.index)
     data = pd.concat([data, encoded_df], axis=1)
-    data.drop(columns=[
-        'Geography',
-        'Gender',
-        'NumOfProducts',
-        
-        ], inplace=True)
-    
-    # Transform data using the pre-fitted scaler
+    data.drop(columns=columns_to_encode, inplace=True)
+    data.drop(columns='Geography_Spain', inplace=True)
+
+    # Упорядочивание колонок
+    ordered_columns = [
+        'CreditScore', 'Age', 'Tenure', 'Balance', 'IsActiveMember',
+        'EstimatedSalary', 'TargetAge', 'Geography_France', 'Geography_Germany',
+        'Gender_Male', 'NumOfProducts_1', 'NumOfProducts_2', 'NumOfProducts_3',
+        'NumOfProducts_4'
+    ]
+    data = data[ordered_columns]
+
+    # Min-Max Scaling
     features = data.columns
+    print(features)
     data[features] = scaler.transform(data[features])
     
     return data
-
 
 @app.route('/')
 def index():
@@ -46,19 +61,20 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get data from the request
     data = request.form.to_dict()
-
     print(data)
-    
-    # Preprocess the input data
     preprocessed_data = preprocess_data(pd.DataFrame([data]))
     
-    # Make prediction
-    prediction = xgb_model.predict(xgb.DMatrix(preprocessed_data))
-    
-    # Return prediction
-    return jsonify({'prediction': prediction.tolist()})
+    prediction_proba = catboost_model.predict_proba(preprocessed_data)
+    prediction = catboost_model.predict(preprocessed_data)
 
+    # Перевод предсказаний
+    prediction_label = 'Останется' if prediction[0] == 0 else 'Покинет'
+
+    # Уверенность в предсказании
+    certainty = max(prediction_proba[0])
+    
+    return jsonify({'prediction': prediction_label, 'certainty': certainty})
+    
 if __name__ == '__main__':
     app.run(debug=True)
